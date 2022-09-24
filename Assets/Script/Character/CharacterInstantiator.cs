30b58c7afcccf5059146d06c5b91ad591a384aa9
Jalil <jputer@uade.edu.ar>
2022-09-24 18:02:59 -0300

    WIP gameplay

--- Assets/Script/Character/CharacterInstantiator.cs
+++ Assets/Script/Character/CharacterInstantiator.cs
@@ -1,21 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using ExitGames.Client.Photon.StructWrapping;
 using Photon.Pun;
 using UnityEngine;
 
-public class CharacterInstantiator : MonoBehaviourPun
+public class CharacterInstantiator : MonoBehaviourPunCallbacks
 {
     // [SerializeField] private SpawnPoint _spawn;
     [SerializeField] private List<SpawnPoint> _spawns; // Temp to TEST
+    
+    
+    
     private void Start()
     {
-        foreach (var _spawn in _spawns)
-        {
-            var character = PhotonNetwork.Instantiate("Character", _spawn.transform.position, _spawn.transform.rotation).GetComponentInChildren<CharacterController>();
+
+        var spawn = _spawns.First(point => !point.Occupied);
+        _spawns.ForEach(p => Debug.Log($"spawn: {p.photonView.ViewID} occupied: {p.Occupied} IsMine: {p.photonView.IsMine}"));
+        var character = PhotonNetwork.Instantiate("Character", spawn.transform.position, spawn.transform.rotation).GetComponentInChildren<CharacterController>();
+    
+        character.SetFlip(spawn.Flip);
+        character.SetVertical(spawn.Vertical);
+
+        spawn.SetOccupied(true);
         
-            character.SetFlip(_spawn.Flip);
-            character.SetVertical(_spawn.Vertical);
-        }
     }
 }
--- Assets/Script/Spawn/SpawnPoint.cs
+++ Assets/Script/Spawn/SpawnPoint.cs
@@ -1,10 +1,43 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Runtime.InteropServices;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
 
-public class SpawnPoint : MonoBehaviour
+public class SpawnPoint : MonoBehaviourPun
 {
     [SerializeField] private bool _flip;
     [SerializeField] private bool _vertical;
+    [SerializeField] private bool _occupied;
+
+    private void Awake()
+    {
+        Debug.Log($"Spawnpoint: {photonView.ViewID} is mine: {photonView.IsMine}");
+        if (photonView.IsMine) return;
+        photonView.RPC(nameof(RequestOccupied), photonView.Owner, PhotonNetwork.LocalPlayer);
+    }
 
     public bool Flip => _flip;
     public bool Vertical => _vertical;
+    public bool Occupied => _occupied;
+
+    public void SetOccupied(bool occupied)
+    {
+        _occupied = occupied;
+        photonView.RPC(nameof(UpdateOccupied), RpcTarget.OthersBuffered, _occupied);
+    }
+
+    [PunRPC]
+    public void UpdateOccupied(bool occupied)
+    {
+        Debug.Log($"Updating spawnpoint {photonView.ViewID} for player {PhotonNetwork.LocalPlayer.NickName}, sending value {occupied}");
+        _occupied = occupied;
+    }
+
+    [PunRPC]
+    public void RequestOccupied(Player player)
+    {
+        Debug.Log($"Requested update spawnpoint {photonView.ViewID} for player {player.NickName}, sending value {_occupied}");
+        photonView.RPC(nameof(UpdateOccupied), player, _occupied);
+    }
 }
